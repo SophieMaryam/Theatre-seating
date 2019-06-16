b2f7614fc3d66c2244bc2572e8a8e40a23975e4e
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-16 18:22:14 +0200

    Add row file and include seat component

---
+++ src/components/Row/Row.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Seats v-for="seat in seats1" :data />
+    <Seats v-for="seat in seats2" :data />
+    <!-- <Seats v-for="seat in seats3" /> -->
+    
+  </div>
+</template>
+
+<script>
+import Seats from "../Seats/Seats";
+import seatData from "../../public/seats.js";
+
+export default {
+  name: 'Row',
+  components: {
+    Seats
+  },
+  data () {
+    return {
+      seats1: seatData.sections[0].rows[2].seats.length,
+      seats2: seatData.sections[1].rows[2].seats.length
+      // seats3: seatData.sections[2].rows[2].seats.length
+    }
+  },
+  methods: {
+    display() {
+      console.log(seatData.ranks);
+    }
+  }
+}
+</script>
