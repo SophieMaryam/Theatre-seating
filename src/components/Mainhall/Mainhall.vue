f97f1d907b2e0f8b77ab653c6ae0e5914ec58d2a
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-16 18:24:54 +0200

    Import components and seatData

--- src/components/Mainhall/Mainhall.vue
+++ src/components/Mainhall/Mainhall.vue
@@ -1,13 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <Row />
+  </div>
 </template>
 
 <script>
+import Row from "../Row/Row";
+import seatData from "../../public/seats.js";
+
 export default {
   name: 'Mainhall',
+  components: {
+    Row
+  },
   data () {
     return {
+      // rows: seatdata.ranks.length
     }
+  },
+  methods: {
   }
 }
 </script>
