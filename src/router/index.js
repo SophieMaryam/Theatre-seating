d3ffa44650a40d542cdd7f573078b1401c8aafac
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-16 16:50:34 +0200

    Temporarily comment file out

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Mainhall from '../components/Mainhall/Mainhall';
+// import Seats from '../components/Seats/Seats';
 
-Vue.use(Router)
+// Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Mainhall',
+//       component: Mainhall
+//     },
+//     {
+//       path: '/',
+//       name: 'Seats',
+//       component: Seats
+//     }
+//   ]
+// })
