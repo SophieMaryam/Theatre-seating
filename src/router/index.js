90409e264dae12600e8c1a1d633b9f4584a656d5
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:57:27 +0200

    Update router name

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,15 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-// import Mainhall from '../components/Mainhall/Mainhall';
-// import Seats from '../components/Seats/Seats';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Sections from '../components/Sections/Sections'
 
-// Vue.use(Router)
+Vue.use(Router)
 
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'Mainhall',
-//       component: Mainhall
-//     },
-//     {
-//       path: '/',
-//       name: 'Seats',
-//       component: Seats
-//     }
-//   ]
-// })
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Sections',
+      component: Sections
+    }
+  ]
+})
