c653c353471424f1b0231741ba680b853610d85a
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:45:11 +0200

    Add group data

---
+++ src/public/groups.js
@@ -0,0 +1,16 @@
+var groups = [
+  { id: '+31611111111', seats: [
+      { section: 'main hall', row: '1', seat: '4' },
+      { section: 'main hall', row: '1', seat: '2' },
+      { section: 'main hall', row: '2', seat: '2' },
+      { section: 'main hall', row: '2', seat: '4' },
+    ]
+  },
+  { id: '+31622222222', seats: [
+      { section: 'main hall', row: '2', seat: '6' },
+      { section: 'main hall', row: '2', seat: '5' },
+    ]
+  },
+ ]
+
+ export default groups;
