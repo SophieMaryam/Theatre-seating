d2bfd72d84728193fcc859aa31a09f9e7ddb4d76
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:49:48 +0200

    Add Row component into file + for loop + bind data

--- src/components/Room/Room.vue
+++ src/components/Room/Room.vue
@@ -1,13 +1,25 @@
  <template>
   <div>
+    <div>
+      {{ name }}
+      <Row v-for="row in rows" 
+      v-bind:key="row.row"
+      :row="row.row"
+      :seats="row.seats"
+      :sectionName="name" />
+    </div>
+    <br/>
   </div>
 </template>
 
 <script>
+import Row from "../Row/Row";
 
 export default {
   name: 'Room',
+  props: ['name', 'rows'],
   components: {
+    Row
   }
 }
 </script>
