9efac09aed8d72a4f5dfd2ce325fa3d156701b36
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:59:52 +0200

    Add props, Seat component and binding, and styling

--- src/components/Row/Row.vue
+++ src/components/Row/Row.vue
@@ -1,46 +1,33 @@
 <template>
-  <div>
-    <div class="rows">
-      <div id="seat-row-1">
-        <Seats v-for="seat in seats1" :data="seat" />
-      </div>
-      <div>
-        <Seats class="seat-row-2" v-for="seat in seats2" :data="seat" />
-      </div>
-      <div>
-        <Seats class="seat-row-3" v-for="seat in seats3" :data="seat" />
-      </div>
-    </div>
+  <div class="row-style">
+      <Seat v-for="seat in seats" 
+      v-bind:key="seat.seat"
+      :seat="seat.seat"
+      :rank="seat.rank"
+      :row="row"
+      :sectionName="sectionName" />
   </div>
 </template>
 
 <script>
-import Seats from "../Seats/Seats";
-import seatData from "../../public/seats.js";
+import Seat from "../Seat/Seat";
 
 export default {
   name: 'Row',
+  props: ['row', 'seats', 'sectionName'],
   components: {
-    Seats
+    Seat
   },
   data () {
     return {
-      seats1: seatData.sections[0].rows[0].seats.length,
-      seats2: seatData.sections[0].rows[1].seats.length,
-      seats3: seatData.sections[0].rows[2].seats.length
     }
-  },
-  methods: {
   }
 }
 </script>
 
 <style>
-.seat-row-2 {
-  float: left;
-}
-
-.rows {
-  /* float: left; */
+.row-style {
+  display: flex;
+  margin-top: 10px;
 }
 </style>
