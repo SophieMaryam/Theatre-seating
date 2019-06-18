72836803b91b836c44da3f7802728e1cf04d3979
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:46:57 +0200

    Add Sections folder and file

---
+++ src/components/Sections/Sections.vue
@@ -0,0 +1,17 @@
+ <template>
+
+</template>
+<script>
+import seatData from "../../public/seats.js";
+
+
+export default {
+  name: 'Sections',
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
