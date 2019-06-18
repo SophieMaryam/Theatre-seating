684ae05ff03aa3c56fd880dcad10802185cc6d25
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:48:58 +0200

    Add Room component and for loop

--- src/components/Sections/Sections.vue
+++ src/components/Sections/Sections.vue
@@ -1,9 +1,15 @@
  <template>
-
+  <div>
+    <div>
+      <Room v-for="section in sections" v-bind:key="section.name"
+      :name="section.name"
+      :rows="section.rows" />
+    </div>
+  </div>
 </template>
 <script>
 import seatData from "../../public/seats.js";
-
+import Room from "../Room/Room";
 
 export default {
   name: 'Sections',
