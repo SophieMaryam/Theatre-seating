721dc791c4ee8989a7c549aef350217d62718c51
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-18 12:50:42 +0200

    Add router-view + remove unused components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-    <Mainhall />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Mainhall from "./components/Mainhall/Mainhall";
 
 export default {
   name: 'App',
-  components: {
-    Mainhall
-  }
 }
 </script>
 
