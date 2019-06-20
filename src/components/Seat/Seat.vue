3d9ceb6b68a94e9b1d92ad1db38dde6bd0ffe00e
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-20 13:51:47 +0200

    Add color assignment functionality

--- src/components/Seat/Seat.vue
+++ src/components/Seat/Seat.vue
@@ -1,55 +1,59 @@
 <template>
-  <div v-if="reserved()">
-    <div class="unavailable"></div>
-  </div>
-  <div v-else>
-    <div class="available"></div>
-  </div>
+  <div class="seat" :style="{ backgroundColor: this.assignColorToSeat() }"></div>
 </template>
 
 <script>
 import groupData from "../../public/groups.js";
 
 export default {
-  name: 'Seat',
-  props: ['seat', 'rank','sectionName', 'row'],
+  name: "Seat",
+  props: ["seat", "rank","sectionName", "row"],
   data () {
     return {
       groupData: groupData
     }
   },
   methods: {
-    reserved: function() {
-      for(let i = 0; i < this.groupData.length; i++) {
+    getIds() {
+      const groupId = this.groupData.map((data) => {
+        return data.id
+      });
+      return groupId
+    },
+    assignColorToId() {
+      const ids = this.getIds();
+      var colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate", "red", "blue", "orange", "yellow"];
+      let coloredIds = [];
+      for(let i=0; i < ids.length ;i++) {
+        coloredIds.push({id: ids[i], color: colors[i]})
+      }
+      return coloredIds
+    },
+    assignColorToSeat() {
+        for (let i = 0; i < this.groupData.length; i++) {
         const seats = this.groupData[i].seats;
-        for(let i = 0; i < seats.length; i++) {
-          if(seats[i].section === this.$props.sectionName &&
-            seats[i].row === this.$props.row &&
-            seats[i].seat === this.$props.seat
-          ) {
-            return true
+        const coloredIds = this.assignColorToId();
+        for (let j = 0; j < seats.length; j++) {
+          if (seats[j].section === this.$props.sectionName &&
+              seats[j].row === this.$props.row &&
+              seats[j].seat === this.$props.seat) {
+                const objectWeNeed = coloredIds.find(
+                  object => object.id === groupData[i].id
+                )
+                return objectWeNeed.color
           }
         }
       }
-      return false
+      return "green";
     }
   }
 }
 </script>
 
 <style>
-  .available {
+  .seat {
     width: 20px;
     height: 20px;
-    background-color: green;
-    margin-right: 10px;
-    display: flex;
-    float: left;
-  }
-  .unavailable {
-    width: 20px;
-    height: 20px;
-    background-color: red;
     margin-right: 10px;
     display: flex;
     float: left;
