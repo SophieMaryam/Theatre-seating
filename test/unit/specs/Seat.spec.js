25bc0832ef44949220b3841a05cedd39da51c07b
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-06-21 09:59:17 +0200

    Add basic unit tests

--- test/unit/specs/Seat.spec.js
+++ test/unit/specs/Seat.spec.js
@@ -1,11 +1,20 @@
-import Vue from 'vue'
-import Seats from '../components/Seat'
+import Vue from "vue";
+import Seat from "../../../src/components/Seat/Seat";
+import { shallowMount } from "@vue/test-utils";
 
-describe('Seat.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Seats)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+describe("Seat.vue", () => {
+  it("should test the Seat component", () => {
+    const wrapper = shallowMount(Seat, {
+      props: {
+        seat: { backgroundColor: "green", seat: "4" },
+        rank: "rank1",
+        sectionName: "main hall",
+        row: "row1"
+      } 
+    })
+    expect(wrapper.contains("div")).toBe(true);
+    expect(wrapper.classes("seat")).toBe(true);
+    expect(wrapper.find(".seat").attributes().style).toBe("background-color: green;"
+    )
   })
-})
+});
