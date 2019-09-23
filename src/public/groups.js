310c5bbf272c66a2e065fcf5dbe87a89f52da316
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-09-23 15:02:25 +0200

    Replace single quotations with double

--- src/public/groups.js
+++ src/public/groups.js
@@ -1,14 +1,14 @@
 var groups = [
-  { id: '+3161111111', seats: [
-      { section: 'main hall', row: '1', seat: '4' },
-      { section: 'main hall', row: '1', seat: '2' },
-      { section: 'main hall', row: '2', seat: '2' },
-      { section: 'main hall', row: '2', seat: '4' },
+  { id: "+3161111111", seats: [
+      { section: "main hall", row: "1", seat: "4" },
+      { section: "main hall", row: "1", seat: "2" },
+      { section: "main hall", row: "2", seat: "2" },
+      { section: "main hall", row: "2", seat: "4" },
     ]
   },
-  { id: '+31622222222', seats: [
-      { section: 'main hall', row: '2', seat: '6' },
-      { section: 'main hall', row: '2', seat: '5' },
+  { id: "+31622222222", seats: [
+      { section: "main hall", row: "2", seat: "6" },
+      { section: "main hall", row: "2", seat: "5" },
     ]
   }
  ]
--- src/public/seats.js
+++ src/public/seats.js
@@ -1,45 +1,45 @@
 var layout = {
-  ranks: [ 'rank1', 'rank2', 'rank3' ],
+  ranks: [ "rank1", "rank2", "rank3" ],
   sections: [
-    { name: 'main hall',
+    { name: "main hall",
       rows: [
-        { row: '1',
+        { row: "1",
           seats: [
-            { seat: '1', rank: 'rank1' },
-            { seat: '3', rank: 'rank1' },
-            { seat: '4', rank: 'rank1' },
-            { seat: '2', rank: 'rank1' }
+            { seat: "1", rank: "rank1" },
+            { seat: "3", rank: "rank1" },
+            { seat: "4", rank: "rank1" },
+            { seat: "2", rank: "rank1" }
           ]
         },
-        { row: '2',
+        { row: "2",
           seats: [
-            { seat: '1', rank: 'rank1' },
-            { seat: '3', rank: 'rank1' },
-            { seat: '5', rank: 'rank1' },
-            { seat: '6', rank: 'rank1' },
-            { seat: '4', rank: 'rank1' },
-            { seat: '2', rank: 'rank1' }
+            { seat: "1", rank: "rank1" },
+            { seat: "3", rank: "rank1" },
+            { seat: "5", rank: "rank1" },
+            { seat: "6", rank: "rank1" },
+            { seat: "4", rank: "rank1" },
+            { seat: "2", rank: "rank1" }
           ]
         },
-        { row: '3',
+        { row: "3",
           seats: [
-            { seat: '1', rank: 'rank2' },
-            { seat: '3', rank: 'rank2' },
-            { seat: '5', rank: 'rank2' },
-            { seat: '6', rank: 'rank2' },
-            { seat: '4', rank: 'rank2' },
-            { seat: '2', rank: 'rank2' }
+            { seat: "1", rank: "rank2" },
+            { seat: "3", rank: "rank2" },
+            { seat: "5", rank: "rank2" },
+            { seat: "6", rank: "rank2" },
+            { seat: "4", rank: "rank2" },
+            { seat: "2", rank: "rank2" }
           ]
         }
       ]
     },
-    { name: '1st balcony',
-      rows: [ { row: '1',
+    { name: "1st balcony",
+      rows: [ { row: "1",
         seats: [
-          { seat: '1', rank: 'rank1' },
-          { seat: '3', rank: 'rank1' },
-          { seat: '4', rank: 'rank1' },
-          { seat: '2', rank: 'rank1' }
+          { seat: "1", rank: "rank1" },
+          { seat: "3", rank: "rank1" },
+          { seat: "4", rank: "rank1" },
+          { seat: "2", rank: "rank1" }
         ]
       } ]
     }
