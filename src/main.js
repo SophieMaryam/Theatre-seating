91945bf68b77a4f03ed9d470fac4a1bb2494d115
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-09-26 15:07:57 +0200

    Add bootstrap and fontawesome import

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router/routes";
+import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
 new Vue({
