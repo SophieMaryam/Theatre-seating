3fbad3e2778ab0fe34c4e1d0add68d6eea2511e8
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-09-26 16:45:56 +0200

    Create SocialMedia.vue

---
+++ src/components/SocialMedia/SocialMedia.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-light">
+    <div class="container">
+      <div class="row py-4 d-flex align-items-center">
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+        </div>
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+          <a class="fb-ic ml-0">
+              <i class="fab fa-facebook black-text mr-4"> </i>
+          </a>
+          <a class="tw-ic">
+              <i class="fab fa-twitter  black-text mr-4"> </i>
+          </a>
+          <a class="gplus-ic">
+              <i class="fab fa-google-plus  black-text mr-4"> </i>
+          </a>
+          <a class="li-ic">
+              <i class="fab fa-linkedin  black-text mr-4"> </i>
+          </a>
+          <a class="ins-ic">
+              <i class="fab fa-instagram  black-text mr-lg-4"> </i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SocialMedia"
+  }
+</script>
