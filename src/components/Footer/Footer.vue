503d7b6772c3bb8768f567fd7a2ed7153bb6092e
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-09-26 16:45:52 +0200

    Create footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="page-footer font-small">
+    <SocialMedia />
+    <div class="bg-shade-black text-white">
+      <div class="container pt-5 mb-4 text-center text-md-left">
+        <div class="row mt-3">
+          <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+            <h6 class="text-uppercase font-weight-bold">
+              <strong>Company name</strong>
+            </h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+              Orius Theatre
+              Oriusstraat 235
+              2134TV
+              Amsterdam
+            </p>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase font-weight-bold">
+              <strong>Useful links</strong>
+            </h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+              <a href="#!" class="link-color">Terms and Agreements</a>
+            </p>
+            <p>
+              <a href="#!" class="link-color">FAQ</a>
+            </p>
+            <p>
+              <a href="#!" class="link-color">Contact</a>
+            </p>
+            <p>
+              <a href="#!" class="link-color">Help</a>
+            </p>
+          </div>
+          <div class="col-md-4 col-lg-3 col-xl-3">
+            <h6 class="text-uppercase font-weight-bold">
+                <strong>Contact</strong>
+            </h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+              <i class="fas fa-home mr-3"></i> Amsterdam, NL
+            </p>
+            <p>
+              <i class="fa fa-envelope mr-3"></i> oriustheatre@gmail.com
+            </p>
+            <p>
+              <i class="fa fa-phone mr-3"></i> + 31 6 274 88 282
+            </p>
+            <p>
+              <i class="fa fa-print mr-3"></i> + 31 6 144 34 817
+            </p>
+          </div>
+          <div class="col-md-4 col-lg-3 col-xl-3"></div>
+        </div>
+      </div>
+      <div class="footer-copyright text-center py-3">© 2019 Copyright:
+        <a href="https://mdbootstrap.com/bootstrap-tutorial/"> Sophie van Wersch</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import SocialMedia from "../SocialMedia/SocialMedia";
+
+export default {
+  name: "Footer",
+  components: {
+    SocialMedia
+  }
+}
+</script>
+
+<style>
+  .bg-shade-black {
+    background-color: #1a1a1a;
+  }
+</style>
+
