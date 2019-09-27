b015db30ae2627bc9bf1e9f1b40b72ae7165e5a1
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-09-27 14:23:38 +0200

    Update landing screen

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -13,51 +13,57 @@
         </div> 
       </div>
     </div>
-    <div>
-      <section id="showings" class="text-center">
-        <div class="container">
-          <h2 class="mb-5 font-weight-bold text-white pt-5">Shows</h2>
-          <div class="row">
-            <div class="col-lg-3 col-md-12 mb-4">
-              <div class="">
-                <img src="../../assets/swanlake.jpg" class="img-fluid" alt="">
-              </div>
-              <button class="theatre-show text-white mt-3">Check out more</button>
+    <section class="text-center pb-4 bg-shade-black">
+      <div class="container">
+        <h2 class="mb-3 font-weight-bold text-white pt-4">Shows</h2>
+        <div class="row">
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/swanlake.jpg" class="img-fluid" alt="">
             </div>
-            <div class="col-lg-3 col-md-12 mb-4">
-              <div class="">
-                <img src="../../assets/dance.jpg" class="img-fluid" alt="">
-              </div>
-              <button class="theatre-show text-white mt-3">Check out more</button>
+            <button class="theatre-show text-white mt-3">Check out more</button>
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/dance.jpg" class="img-fluid" alt="">
             </div>
-            <div class="col-lg-3 col-md-12 mb-4">
-              <div class="">
-                <img src="../../assets/band.jpg" class="img-fluid" alt="">
-              </div>
-              <button class="theatre-show text-white mt-3">Check out more</button>
+            <button class="theatre-show text-white mt-3">Check out more</button>
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/band.jpg" class="img-fluid" alt="">
             </div>
-            <div class="col-lg-3 col-md-12 mb-4">
-              <div class="">
-                <img src="../../assets/acting.jpg" class="img-fluid" alt="">
-              </div>
-              <button class="theatre-show text-white mt-3">Check out more</button>
+            <button class="theatre-show text-white mt-3">Check out more</button>
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/acting.jpg" class="img-fluid" alt="">
             </div>
+            <button class="theatre-show text-white mt-3">Check out more</button>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+    <div class="bg-color">
+      <h1 class="text-center text-white pt-5">Food and Drink</h1>
     </div>
+    <div id="meals" class="image"></div>
+    <div class="bg-color">
+      <h1 class="text-center text-white pt-5">Our History</h1>
+      <p></p>
+    </div>
+    <div id="history" class="image"></div>
+    <div class="bg-color">
+      <h1 class="text-center text-white pt-5">Shop</h1>
+      <p></p>
+    </div>
+    <div id="shop" class="image"></div>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Navbar from "../Navbar/Navbar";
-
-export default {
-  name: "Home",
-  components: {
-    Navbar
-  }
-}
+module.exports = require("./Home.js");
 </script>
 
 <style scoped>
@@ -71,12 +77,37 @@ export default {
     height: 100vh;
   }
 
-  #showings {
+  .bg-shade-black {
     background-color: #1a1a1a;
   }
 
   .theatre-show {
     background-color: #8B0000;
     height: 50px;
+  }
+
+  .bg-color {
+    background-color: #8B0000;
+    height: 150px;
+  }
+
+  #meals {
+    background: url("../../assets/food.jpg") no-repeat center center fixed;
+  }
+
+  .image {
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    height: 40vh;
+  }
+
+  #history {
+    background: url("../../assets/history.jpg") no-repeat center center fixed;
+  }
+
+  #shop {
+    background: url("../../assets/shop.jpg") no-repeat center center fixed;
   }
 </style>
