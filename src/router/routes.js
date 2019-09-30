e4c30b43fe4e74c1f06107cccb04d84307bf9bb0
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-09-30 13:35:53 +0200

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "../components/Home/Home";
 import Sections from "../components/Sections/Sections";
+import Login from "../components/Login/Login";
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export const routes = [
     path: "/theatre",
     name: "Sections",
     component: Sections
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   }
 ]
 
