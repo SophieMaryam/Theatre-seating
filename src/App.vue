eb9905f4da4811c935cd28a0a51f77896f17b9d9
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-01 14:12:18 +0200

    Add Footer and Navbar to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
-export default {
-  name: "App"
-}
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+  export default {
+    name: "App",
+    components: {
+      Navbar,
+      Footer
+    }
+  }
 </script>
