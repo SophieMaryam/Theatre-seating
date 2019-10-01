8ba9fe6e266ff60740b696f9f3309ac8b63a0ed1
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-01 14:11:15 +0200

    Add onSubmit method

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,19 +1,28 @@
 <template>
- <div>
-   <p>Hi</p>
-   <!-- <form class="login" @submit.prevent="login">
-     <h1>Sign in</h1>
-     <label>User name</label>
-     <input required v-model="username" type="text" placeholder="Username"/>
-     <label>Password</label>
-     <input required v-model="password" type="password" placeholder="Password"/>
-     <hr/>
-     <button type="submit">Login</button>
-   </form> -->
- </div>
+  <div id="login">
+  </div>
 </template>
 
-<script src="./script.js">
-  // module.exports = require("./Login.js");
+<script>
+  export default {
+    name: "Login",
+    data () {
+      return {
+        email: "",
+        password: ""
+      }
+    },
+    methods: {
+      onSubmit () {
+        const formData = {
+          email: this.email,
+          password: this.password,
+        }
+        console.log(formData)
+      }
+    }
+  }
 </script>
 
+<style scoped>
+</style>
