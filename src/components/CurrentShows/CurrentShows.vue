b32315805e7565f8a113f8aee9e17cef8fc0211c
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-01 14:08:28 +0200

    Create CurrentShows.vue and add section from Home.vue
    Separating code into components

---
+++ src/components/CurrentShows/CurrentShows.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="text-center pb-4 bg-shade-black">
+      <div class="container">
+        <h2 class="mb-3 font-weight-bold text-white pt-4">Shows</h2>
+        <div class="row">
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/swanlake.jpg" class="img-fluid" alt="">
+            </div>
+            <button class="check-btn text-white mt-3">Find Tickets</button>
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/dance.jpg" class="img-fluid" alt="">
+            </div>
+            <button class="check-btn text-white mt-3">Find Tickets</button>
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/band.jpg" class="img-fluid" alt="">
+            </div>
+            <button class="check-btn text-white mt-3">Find Tickets</button>
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+            <div class="">
+              <img src="../../assets/acting.jpg" class="img-fluid" alt="">
+            </div>
+            <button class="check-btn text-white mt-3">Find Tickets</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "CurrentShows"
+}
+</script>
+
+<style scoped>
+  .check-btn {
+    background-color: #8B0000;
+    height: 50px;
+    border: #8B0000;
+    padding: 10px;
+    border-radius: 5%;
+  }
+
+  .check-btn:hover {
+    background-color: rgb(170, 2, 2);
+    border: rgb(170, 2, 2);
+  }
+
+  .bg-shade-black {
+    background-color: #1a1a1a;
+  }
+</style>
+
+
