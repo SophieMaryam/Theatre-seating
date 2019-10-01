cfb5ffcb858086c9e869c36bbb3a6f2945b5f73d
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-01 14:11:41 +0200

    Update login form

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,5 +1,31 @@
 <template>
   <div id="login">
+    <div class="login-form">
+      <h1 class="text-center">Register</h1>
+      <form @submit.prevent="onSubmit">
+        <div class="input">
+          <label for="email">Mail</label>
+          <input
+                  type="email"
+                  id="email"
+                  v-model="email">
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input
+                  type="password"
+                  id="password"
+                  v-model="password">
+        </div>
+        <div class="submit pt-4 pb-2 text-center">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div class="pt-2 text-center">
+        Not a user? Register
+        <router-link to="/register">here</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
