15f8e22cc45b75b89dcd70a8779fb0054af784f7
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-01 14:12:54 +0200

    Create and add register box

---
+++ src/components/Register/Register.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="register">
+    <div class="register-form">
+      <h1 class="text-center">Login</h1>
+      <form @submit.prevent="onSubmit">
+        <div class="input">
+          <label for="email">Mail</label>
+          <input
+                  type="email"
+                  id="email"
+                  v-model="email">
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input
+                  type="password"
+                  id="password"
+                  v-model="password">
+        </div>
+        <div class="input">
+          <label for="confirm-password">Confirm Password</label>
+          <input
+                  type="password"
+                  id="confirm-password"
+                  v-model="confirmPassword">
+        </div>
+        <div class="input inline">
+          <input type="checkbox" id="terms" v-model="terms">
+          <label for="terms">Accept Terms of Use</label>
+        </div>
+        <div class="submit pt-4 text-center">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div class="pt-2 text-center">
+        <p>Already have an account? Login
+          <router-link to="/login">here</router-link>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Register",
+    data () {
+      return {
+        email: "",
+        password: "",
+        confirmPassword: "",
+        terms: false
+      }
+    },
+    methods: {
+      onSubmit () {
+        const formData = {
+          email: this.email,
+          password: this.password,
+          confirmPassword: this.confirmPassword,
+          terms: this.terms
+        }
+        console.log(formData)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  #register {
+    margin-top: 100px;
+    margin-bottom: 100px;
+  }
+
+  .register-form {
+    width: 400px;
+    margin: 30px auto;
+    border: 1px solid #eee;
+    padding: 20px;
+    box-shadow: 0 2px 3px #ccc;
+  }
+
+  .input {
+    margin: 10px auto;
+  }
+
+  .input label {
+    display: block;
+    color: #4e4e4e;
+    margin-bottom: 6px;
+  }
+
+  .input.inline label {
+    display: inline;
+  }
+
+  .input input {
+    font: inherit;
+    width: 100%;
+    padding: 6px 12px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+  }
+
+  .input.inline input {
+    width: auto;
+  }
+
+  .input input:focus {
+    outline: none;
+    border: 1px solid #8B0000;
+    background-color: #eee;
+  }
+
+  .input select {
+    border: 1px solid #ccc;
+    font: inherit;
+  }
+
+  .submit button {
+    border: 1px solid #8B0000;
+    color: #8B0000;
+    padding: 10px 20px;
+    font: inherit;
+    cursor: pointer;
+  }
+
+  .submit button:hover,
+  .submit button:active {
+    background-color: #8B0000;
+    color: white;
+  }
+
+  .submit button[disabled],
+  .submit button[disabled]:hover,
+  .submit button[disabled]:active {
+    border: 1px solid #ccc;
+    background-color: transparent;
+    color: #ccc;
+    cursor: not-allowed;
+  }
+</style>
