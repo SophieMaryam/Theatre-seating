088c8c427ac04641002f8654fe40c4652d396674
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-02 11:21:08 +0200

    Create AvailableShows.vue

---
+++ src/components/AvailableShows/AvailableShows.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="background pt-5 pb-5">
+  <div class="container col-7">
+    <input class="form-control mb-1" type="search" placeholder="Search" aria-label="Search">
+    <div class="card mb-1">
+      <div class="card-body d-flex">
+        <div class="image" style="background-image:url(../../assets/swanlake.jpg)"></div>
+        <div>
+          <p>Orius Hall presents</p>
+          <h2>The Princess Swan</h2>
+          <p>Tuesday 22 - Wednesday 20 November 2019
+            <span>Auditorium</span>
+          </p>
+          <a href="#" class="btn text-white">Find Tickets</a>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-1">
+      <div class="card-body d-flex">
+        <div class="image" style="background-image:url('../../assets/swanlake.jpg')"></div>
+        <div>
+          <p>Orius Hall presents</p>
+          <h2>The Princess Swan</h2>
+          <p>Tuesday 22 - Wednesday 20 November 2019
+            <span>Auditorium</span>
+          </p>
+          <a href="#" class="btn text-white">Find Tickets</a>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-1">
+      <div class="card-body d-flex">
+        <div class="image" style="background-image:url('../../assets/swanlake.jpg')"></div>
+        <div>
+          <p>Orius Hall presents</p>
+          <h2>The Princess Swan</h2>
+          <p>Tuesday 22 - Wednesday 20 November 2019
+            <span>Auditorium</span>
+          </p>
+          <a href="#" class="btn text-white">Find Tickets</a>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-1">
+      <div class="card-body d-flex">
+        <div class="image" style="background-image:url('../../assets/swanlake.jpg')"></div>
+        <div>
+          <p>Orius Hall presents</p>
+          <h2>The Princess Swan</h2>
+          <p>Tuesday 22 - Wednesday 20 November 2019
+            <span>Auditorium</span>
+          </p>
+          <a href="#" class="btn text-white">Find Tickets</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "CurrentShows"
+}
+</script>
+
+<style scoped>
+  .background {
+    background-color: #8B0000;
+  }
+
+  .image {
+    height: 150px;
+    width: 250px;
+  }
+
+  .btn {
+    background-color:#8B0000;
+  }
+
+   .btn:hover {
+    background-color: rgb(170, 2, 2);
+    border: rgb(170, 2, 2);
+  }
+</style>
