679eeea1aba6a1f42e2871033538f6ec90531c54
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-07 14:37:03 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Vuex from "vuex";
+import firebase from "firebase";
 import { router } from "./router/routes";
 import { store } from "./store/store";
 import BootstrapVue from "bootstrap-vue";
@@ -9,17 +10,27 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPlaceOfWorship, faSignInAlt, faSearch, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { config } from "./common/config";
 
 Vue.use(BootstrapVue);
 Vue.use(Vuex)
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+let app = "";
+firebase.initializeApp(config.firebase);
+
+
 
 library.add(faPlaceOfWorship, faSignInAlt, faSearch, faShoppingBag);
-Vue.component("font-awesome-icon", FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 
-new Vue({
-  store,
-  router,
-  render: h => h(App)
-}).$mount("#app");
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      store,
+      router,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
