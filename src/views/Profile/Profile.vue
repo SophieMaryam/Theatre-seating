0eba3096af031e7368aead25f5b3326b347024b8
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-07 14:36:17 +0200

    Create Profile.vue

---
+++ src/views/Profile/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Profile</h1>
+    <button @click="logout">Sign Out</button>
+  </div>
+</template>
+
+<script>
+module.exports = require("./Profile.js");
+</script>
+
+<!-- <style src="./Profile.css">
+</style> -->
