360488a3223d255180b1af4386171d060bdf2c81
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-07 16:28:42 +0200

    Create and move social media auth into separate component

---
+++ src/components/LoginSocialMedia/LoginSocialMedia.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+
+export default {
+  name: "LoginSocialMedia",
+  props: {
+    onClick: {
+      type: Function,
+      required: false
+    }
+  }
+}
---
+++ src/components/LoginSocialMedia/LoginSocialMedia.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button class="social-button" @click="onClick" type="submit">
+      <img src="../../assets/google-logo.png" alt="Google logo">
+    </button>
+  </div>
+</template>
+
+<script>
+  module.exports = require("./LoginSocialMedia.js");
+</script>
+
+<style src="./LoginSocialMedia.css">
+</style>
