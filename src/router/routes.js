02236ac57c78edbf7576d8b82e93bfb256c5f1af
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-07 14:37:43 +0200

    Create protected paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,13 @@
+import firebase from "firebase";
 import Vue from "vue";
 import Router from "vue-router";
+
 import Home from "../components/Home/Home.vue";
 import Sections from "../components/Sections/Sections.vue";
 import Login from "../components/Login/Login.vue";
 import Register from "../components/Register/Register.vue";
 import AvailableShows from "../components/AvailableShows/AvailableShows.vue";
+import Profile from "../views/Profile/Profile.vue";
 
 Vue.use(Router);
 
@@ -30,14 +33,45 @@ export const routes = [
     component: Login
   },
   {
+    path: "*",
+    redirect: "/login",
+  },
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
     path: "/tickets",
     name: "AvailableShows",
     component: AvailableShows
-  }
-
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+    meta: {
+      requiresAuth: true
+    }
+  },
 ]
 
 export const router = new Router({
   mode: "history",
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) {
+    next("login");
+  }
+  else if (!requiresAuth && currentUser) {
+    next("profile");
+  } 
+  else {
+    next();
+  }
+});
+
