86c99239bef22c50e2eb26ec24281edbb5d12834
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-08 13:46:48 +0200

    Add login dropdown

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -3,44 +3,60 @@
     <nav class="navbar navbar-expand-lg">
       <div class="container">
         <a class="navbar-brand" href="/"> 
-          <font-awesome-icon icon="place-of-worship" class="orius" /> Orius Theatre </a>
-          <button 
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse" data-target="#navbarcontent" aria-controls="navbarcontent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div 
-            class="collapse navbar-collapse" 
-            id="navbarcontent text-center"
-          >
-            <ul class="navbar-nav ml-auto mr-auto">
-              <li class="nav-item">
-                <router-link class="nav-link text-uppercase font-weight-bold" to="/tickets"> What's On
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-uppercase font-weight-bold" href="#"> About Orius </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-uppercase font-weight-bold" href="#">
-                  Shop
-                </a>
-              </li>
-              <li>
-                <a class="nav-link text-uppercase font-weight-bold" href="#">Contact</a>
-              </li>
-            </ul>
+          <font-awesome-icon icon="place-of-worship" class="orius" /> Orius Theatre 
+        </a>
+        <button 
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse" data-target="#navbarcontent" aria-controls="navbarcontent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div 
+          class="collapse navbar-collapse" 
+          id="navbarcontent text-center"
+        >
+          <ul class="navbar-nav ml-auto mr-auto">
+            <li class="nav-item">
+              <router-link class="nav-link text-uppercase font-weight-bold" to="/tickets"> What's On
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-uppercase font-weight-bold" href="#"> About Orius </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-uppercase font-weight-bold" href="#">
+                Shop
+              </a>
+            </li>
+            <li>
+              <a class="nav-link text-uppercase font-weight-bold" href="#">Contact</a>
+            </li>
+          </ul>
           <form class="form-inline">
             <input v-if="searchVisible" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <font-awesome-icon @click.prevent="search" icon="search" class="icons" />
             <font-awesome-icon icon="shopping-bag" class="icons" />
-            <router-link to="/login">
-              <font-awesome-icon icon="sign-in-alt" class="icons" />
-            </router-link>
+            <b-dropdown id="button">
+              <template slot="button-content">
+                <font-awesome-icon class="icons" icon="sign-in-alt" />
+              </template>
+              <b-dropdown-item class="dropdown">
+                <router-link to="/login" class="link">Login
+                </router-link>
+              </b-dropdown-item>
+              <b-dropdown-divider></b-dropdown-divider>
+              <b-dropdown-item>
+                <router-link to="/register" class="link">Registeration
+                </router-link>
+              </b-dropdown-item>
+              <b-dropdown-divider></b-dropdown-divider>
+              <b-dropdown-item>
+                <router-link to="/profile" class="link">My Account
+                </router-link>
+              </b-dropdown-item>
+            </b-dropdown>
           </form>
         </div>
       </div>
