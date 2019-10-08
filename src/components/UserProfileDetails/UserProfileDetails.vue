7553973d0cfa988d7f597c9f1d7c7837622b0911
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-08 14:20:56 +0200

    Add upload photo feature + scoped styling

--- src/components/UserProfileDetails/UserProfileDetails.vue
+++ src/components/UserProfileDetails/UserProfileDetails.vue
@@ -1,13 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <div class="col-lg-4 order-lg-1 text-center">
+      <img src="//placehold.it/150" class="mx-auto img-fluid img-circle d-block" alt="avatar">
+      <h6 class="mt-2">Upload a different photo</h6>
+      <label class="custom-file">
+        <input type="file" id="file" class="custom-file-input">
+        <span class="custom-file-control">Choose file</span>
+      </label>
+    </div>
+  </div>
 </template>
+
+<style scoped src="./UserProfileDetails.css">
+</style>
 
 <script>
   module.exports = require("./UserProfileDetails.js");
 </script>
-
-
-<style src="./UserProfileDetails.css">
-</style>
 
 
