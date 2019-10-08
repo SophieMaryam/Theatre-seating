70655d057a44f781151de478f75f57eedfc23a2a
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-08 13:47:51 +0200

    Add components

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import firebase from "firebase";
+import UserProfileDetails from "../../components/UserProfileDetails/UserProfileDetails.vue";
+import Settings from "../../components/Settings/Settings.vue";
 
 export default Vue.extend({
   name: "Profile",
+  components: {
+    UserProfileDetails,
+    Settings
+  },
   methods: {
     logout() {
       firebase.auth().signOut()
