bb4a64f6bcb204b51bd21080b171eae2fcc9e766
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-29 11:45:24 +0100

    Add search filtering

--- src/components/AvailableShows/AvailableShows.js
+++ src/components/AvailableShows/AvailableShows.js
@@ -1,5 +1,26 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "CurrentShows"
+  name: "CurrentShows",
+  data() {
+    return {
+      availableShowsData: [],
+      availableShows: [],
+      search: { filter: null, text: "" }
+    }
+  },
+  methods: {
+    search_text() {
+      var inside = this;
+      this.availableShows = this.availableShowsData.filter(function(allShows) {
+        if (
+          allShows.show
+            .toLowerCase()
+            .indexOf(inside.search.text.toLowerCase()) != "-1"
+        ) {
+          return allShows;
+        }
+      });
+    },
+  }
 });
