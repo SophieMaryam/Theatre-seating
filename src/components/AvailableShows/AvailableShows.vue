7e6d68b98ed8d6444dcacf9c9321e7da90aba2c2
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-10-29 11:45:15 +0100

    Temporarily comment out code

--- src/components/AvailableShows/AvailableShows.vue
+++ src/components/AvailableShows/AvailableShows.vue
@@ -1,19 +1,19 @@
 <template>
 <div class="background pt-5 pb-5">
   <div class="container col-7">
-    <b-form-input
+    <!-- <b-form-input
       @input="search_text()"
       v-model="search.text"
       class="form-control mb-1"
       type="text" 
       placeholder="Search" 
       aria-label="Search"
-    ></b-form-input>
-    <div>
+    ></b-form-input> -->
+    <!-- <div>
       <b-form-select @input="sort()" v-model="search.filter" :options="options">
         <font-awesome-icon icon="search" />
       </b-form-select>
-    </div>
+    </div> -->
     <div class="card mb-1">
       <div class="card-body d-flex">
         <div class="image" style="background-image:url(../../assets/swanlake.jpg)"></div>
