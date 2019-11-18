2c08c3cf7c42b8aa6fb9baaa71f0b212d6264046
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-11-18 10:19:19 +0100

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,58 +1,93 @@
 <template>
   <div class="page-footer font-small">
-    <SocialMedia />
-    <div class="bg-shade-black text-white">
-      <div class="container-fluid pt-5 mb-4 text-center text-md-left">
+  <hr class="container footer-hr"></hr>
+    <div class="text-black">
+      <div class="container container-fluid pt-5 mb-4 text-center text-md-left">
         <div class="row mt-3">
           <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
             <h6 class="text-uppercase font-weight-bold">
-              <strong>Company name</strong>
+              <strong>Contact</strong>
             </h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
             <p>
-              Orius Theatre
-              Oriusstraat 235
-              2134TV
-              Amsterdam
-            </p>
-          </div>
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 class="text-uppercase font-weight-bold">
-              <strong>Useful links</strong>
-            </h6>
-            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-            <p>
-              <a href="#!" class="link-color">Terms and Agreements</a>
+              Visiting address
+              Amstel 115 - 125
+              1018 EM Amsterdam
             </p>
             <p>
-              <a href="#!" class="link-color">FAQ</a>
+              Postal address
+              Postbus 20202
+              1000 HE Amsterdam
             </p>
             <p>
-              <a href="#!" class="link-color">Contact</a>
-            </p>
-            <p>
-              <a href="#!" class="link-color">Help</a>
+              Opening hours ticket office
+              Mon to Sun 4 pm to 6 pm
+              (or until start of show)
+              On Saturdays, Sundays and holidays without a program, Orius ticket office is closed.
             </p>
           </div>
           <div class="col-md-4 col-lg-3 col-xl-3">
             <h6 class="text-uppercase font-weight-bold">
-                <strong>Contact</strong>
+              <strong>Ticket Sales</strong>
             </h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-            <p>
-              <i class="fas fa-home mr-3"></i> Amsterdam, NL
+            <p> <strong> Online </strong>
+            <br>
+            <router-link to="tickets"> View the complete list of shows </router-link>
             </p>
-            <p>
-              <i class="fa fa-envelope mr-3"></i> oriustheatre@gmail.com
-            </p>
-            <p>
-              <i class="fa fa-phone mr-3"></i> + 31 6 274 88 282
-            </p>
-            <p>
-              <i class="fa fa-print mr-3"></i> + 31 6 144 34 817
-            </p>
+            <p> <strong> By phone </strong>
+            <br>
+            +31 (0)628354238
+            <br>
+            Mon to Fri 9am to 9pm
+            <br>
+            Sat to Sun 10am to 8pm </p>
           </div>
-          <div class="col-md-4 col-lg-3 col-xl-3"></div>
+          <div class="col-md-3 col-lg-2 col-xl-3">
+            <h6 class="text-uppercase font-weight-bold">
+              <strong> Follow Orius Online </strong>
+            </h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <div class="col-md-6 col-lg-7 text-center text-md-right">
+              <a class="fb-ic ml-0">
+                <i class="fab fa-facebook black-text mr-4"> </i>
+              </a>
+              <a class="tw-ic">
+                <i class="fab fa-twitter black-text mr-4"> </i>
+              </a>
+              <a class="gplus-ic">
+                <i class="fab fa-google-plus black-text mr-4"> </i>
+              </a>
+              <a class="li-ic">
+                <i class="fab fa-linkedin  black-text mr-4"> </i>
+              </a>
+              <a class="ins-ic">
+                <i class="fab fa-instagram  black-text mr-lg-4"> </i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-3">
+            <h6 class="text-uppercase font-weight-bold">
+              <strong>Useful links</strong>
+            </h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <div class="col-md-6 col-lg-7 text-center text-md-right">
+              <ul class="useful-links">
+                <li>
+                  <a href="#!" class="link-color">Terms and Agreements</a>
+                </li>
+                <li>
+                  <a href="#!" class="link-color">FAQ</a>
+                </li>
+                <li>
+                  <a href="#!" class="link-color">Contact</a>
+                </li>
+                <li>
+                  <a href="#!" class="link-color">Help</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
       <div class="footer-copyright text-center py-3">© 2019 Copyright:
