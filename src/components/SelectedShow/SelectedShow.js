bb611b6722c9b7de1c3213a8360a28dfb3fcdcef
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-03 14:56:48 +0100

    Add show details

--- src/components/SelectedShow/SelectedShow.js
+++ src/components/SelectedShow/SelectedShow.js
@@ -4,20 +4,87 @@ export default Vue.extend({
   name: "SelectedShow",
   data() {
     return {
-      rows: [
-        { id: 1, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00" },
-        { id: 2, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00" },
-        { id: 3, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00"},
-        { id: 4, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00" }
+      showName: this.$route.params.title,
+      showImg: this.$route.params.img,
+      showIndex: this.$route.params.index,
+      readMore: false,
+      allShows: [ 
+        {
+          name: "Swan Lake",
+          info: [ 
+            { id: 1, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00" },
+            { id: 2, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00" },
+            { id: 3, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00"},
+            { id: 4, date: "wed 21 oct", time: "20:00", cost: "from EU 25,00" }
+          ]
+        },
+        {
+          name: "Drummers",
+          info: [ 
+            { id: 1, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00" },
+            { id: 2, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00" },
+            { id: 3, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00"},
+            { id: 4, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00" }
+          ]
+        },
+        {
+          name: "Symphony II",
+          info: [ 
+            { id: 1, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00" },
+            { id: 2, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00" },
+            { id: 3, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00"},
+            { id: 4, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00" }
+          ]
+        },
+        {
+          name: "Behind Closed Curtains",
+          info: [ 
+            { id: 1, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00" },
+            { id: 2, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00" },
+            { id: 3, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00"},
+            { id: 4, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00" }
+          ]
+        }
       ]
     }
   },
   mounted() {
-    this.displaySelectedShowInfo;
+    this.displaySelectedShowInfo();
+    // this.getAllShowData();
   },
   methods: {
     displaySelectedShowInfo() {
-      console.log(this.$route)
+      if(this.$route.params.title == "Swan Lake") {
+        // return this.showInfo;
+        
+      }
+    },
+    myFunction() {
+      this.readMore = true;
+      // var dots = document.getElementById("dots");
+      // var moreText = document.getElementById("more");
+      // var btnText = document.getElementById("btn");
+    
+      // if (dots.style.display === "none") {
+      //   dots.style.display = "inline";
+      //   btnText.innerHTML = "Read more";
+      //   moreText.style.display = "none";
+      // } else {
+      //   dots.style.display = "none";
+      //   btnText.innerHTML = "Read less";
+      //   moreText.style.display = "inline";
+      // }
+    },
+    routerPush() {
+      this.$router.push({
+        name: "Sections"
+      })
     }
+    // getAllShowData() {
+    //   return this.allShowData = this.shows.map(show => {
+    //     this.allShowNames = show.name;
+    //     this.showInfo = show.info;
+    //   });
+    // }
   }
 });
