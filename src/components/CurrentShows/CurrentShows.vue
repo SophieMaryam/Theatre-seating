23075e75bf3ecddd89b5dfe3da8ac54d2fccacfa
SophieMaryam <sophievanwersch@hotmail.com>
2019-12-05 10:16:04 +0100

    Removed unused code

--- src/components/CurrentShows/CurrentShows.vue
+++ src/components/CurrentShows/CurrentShows.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
-    <div class="text-center pb-4 bg-shade-black">
+    <div class="text-center pb-4">
       <div class="container">
         <h2 class="mb-3 font-weight-bold text-white pt-4">Shows</h2>
         <div class="row">
           <div class="col-lg-3 col-md-12 mb-4">
-            <div class="">
+            <div>
               <img src="../../assets/swanlake.jpg" class="img-fluid" alt="">
             </div>
             <button class="check-btn text-white mt-3">Find Tickets</button>
           </div>
           <div class="col-lg-3 col-md-12 mb-4">
-            <div class="">
+            <div>
               <img src="../../assets/dance.jpg" class="img-fluid" alt="">
             </div>
             <button class="check-btn text-white mt-3">Find Tickets</button>
           </div>
           <div class="col-lg-3 col-md-12 mb-4">
-            <div class="">
+            <div>
               <img src="../../assets/band.jpg" class="img-fluid" alt="">
             </div>
             <button class="check-btn text-white mt-3">Find Tickets</button>
           </div>
           <div class="col-lg-3 col-md-12 mb-4">
-            <div class="">
+            <div>
               <img src="../../assets/acting.jpg" class="img-fluid" alt="">
             </div>
             <button class="check-btn text-white mt-3">Find Tickets</button>
