8c1ea6fc5f60472147842c1f175f5362a44000c6
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-09 14:24:04 +0100

    Add settings data and methods

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,5 +1,35 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "UserProfileDetails"
+  name: "UserProfileDetails",
+  data(){
+    return {
+        profile: {
+          name:null,
+          phone:null,
+          address:null,
+          postcode:null
+        },
+        account:{
+            name:null,
+            email:null,
+            photoUrl:null,
+            emailVerified:null,
+            password:null,
+            confirmPassword:null,
+            uid:null
+        }       
+    }
+  },
+  methods: {
+    updateProfile() {
+      
+    },
+    uploadImage() {
+      console.log("hi");
+    },
+    resetPassword() {
+      console.log("To reset");
+    }
+  }
 });
