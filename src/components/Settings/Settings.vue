00e24d376b1e3e37718c7e0f9b87313026591400
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-09 14:23:55 +0100

    Update settings form

--- src/components/Settings/Settings.vue
+++ src/components/Settings/Settings.vue
@@ -1,35 +1,198 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <form role="form">
-        <div class="form-group col-xs-10 col-sm-4 col-md-4 col-lg-12">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+  <div class="products">
+    <div class="container">
+      <div class="intro h-100">
+        <div class="row h-100 align-items-center">
+          <div class="col-md-6 ml-3">
+            <h3>Profile settings</h3>
+
+            <p>Change your profile settings here</p>
+          </div>
+          <div class="col-md-5">
+            <img src="/img/svg/profile.svg" width="300" alt class="img-fluid" />
+          </div>
         </div>
-        <div class="form-group col-xs-10 col-sm-4 col-md-4 col-lg-12">
-            <label for="exampleInputEmail1">Name</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Name">
+      </div>
+
+      <div class="profile-content">
+        <!-- <ul class="nav nav-pills ml-3" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a
+              class="nav-link active"
+              id="profile-tab"
+              data-toggle="tab"
+              href="#profile"
+              role="tab"
+              aria-controls="profile"
+              aria-selected="true"
+            >Profile</a>
+          </li>
+
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="account-tab"
+              data-toggle="tab"
+              href="#account"
+              role="tab"
+              aria-controls="account"
+              aria-selected="false"
+            >Account settings</a>
+          </li>
+        </ul> -->
+
+        <div class="tab-content" id="myTabContent">
+          <div
+            class="tab-pane fade show active pt-3"
+            id="profile"
+            role="tabpanel"
+            aria-labelledby="profile-tab"
+          >
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      name
+                      v-model="profile.name"
+                      placeholder="Full name"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="profile.phone"
+                      placeholder="Phone"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="profile.address"
+                      placeholder="Address"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-8">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="profile.postCode"
+                      placeholder="Postcode"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input
+                      type="submit"
+                      @click="updateProfile"
+                      value="Save Changes"
+                      class="btn btn-primary w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="tab-pane fade pt-3"
+            id="account"
+            role="tabpanel"
+            aria-labelledby="account-tab"
+          >
+            <div class="container">
+              <div class="row">
+                <div class="col-md-">
+                  <div
+                    class="alert alert-info"
+                  >Please use the Reset password email button for reseting the password. The form doens't work currently</div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="account.name"
+                      placeholder="User name"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="account.email"
+                      placeholder="Email address"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="account.password"
+                      placeholder="New password"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      v-model="account.confirmPassword"
+                      placeholder="Confirm password"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input type="file" @change="uploadImage" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input type="submit" value="Save Changes" class="btn btn-primary w-100" />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input
+                      type="button"
+                      @click="resetPassword"
+                      value="Reset password email"
+                      class="btn btn-success w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="clearfix"></div>
-        <div class="form-group col-xs-10 col-sm-4 col-md-4 col-lg-12">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-group col-xs-10 col-sm-4 col-md-4 col-lg-12">
-            <label for="exampleInputPassword1">Confirm Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
-        </div>
-        <div class="clearfix"></div>
-        <div class="form-group col-xs-10 col-sm-10 col-md-4 col-lg-12">
-          <label for="exampleInputFile">File input</label>
-          <input type="file" id="exampleInputFile">
-        </div>
-        <div class="clearfix"></div>
-        <div class="col-xs-10 col-sm-4 col-md-4 col-lg-12">
-            <button type="submit" class="btn btn-default">Submit</button>
-        </div>
-      </form>
-    <div class="clearfix"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,6 +201,6 @@
 </style>
 
 <script>
-  module.exports = require("./Settings.js");
+module.exports = require("./Settings.js");
 </script>
 
