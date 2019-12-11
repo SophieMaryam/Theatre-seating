17c979d2c3760be800157f3431583e41db8a9ec1
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-11 10:35:04 +0100

    Retrieve user name from firestore db

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,35 +1,51 @@
 import Vue from "vue";
+import firebase from "firebase";
+import { db } from "../../firebase";
 
 export default Vue.extend({
-  name: "UserProfileDetails",
-  data(){
+  name: "Settings",
+  data() {
     return {
-        profile: {
-          name:null,
-          phone:null,
-          address:null,
-          postcode:null
-        },
-        account:{
-            name:null,
-            email:null,
-            photoUrl:null,
-            emailVerified:null,
-            password:null,
-            confirmPassword:null,
-            uid:null
-        }       
+      profile: {
+        name: null,
+        phone: null,
+        address: null,
+        postcode: null
+      },
+      account: {
+        name: null,
+        email: null,
+        photoUrl: null,
+        emailVerified: null,
+        password: null,
+        confirmPassword: null,
+        uid: null
+      }
+    }
+  },
+  mounted() {
+    this.doThat();
+  },
+  firestore() {
+    const user = firebase.auth().currentUser;
+    console.log(user);
+    return {
+      profile: db.collection("profiles").doc(user.uid)
     }
   },
   methods: {
     updateProfile() {
-      
+
     },
     uploadImage() {
       console.log("hi");
     },
     resetPassword() {
       console.log("To reset");
+    },
+    doThat() {
+      const user = firebase.auth().currentUser;
+      console.log(user.uid)
     }
   }
 });
