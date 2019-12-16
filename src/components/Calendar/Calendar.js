f792589b2f886f6ab2480b9ed0a2d598b4ccd8ff
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-16 15:25:11 +0100

    Remove unused code + include json import

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,54 +1,19 @@
 import Vue from "vue";
+import allShowsDataJSON from "../../common/showList.json";
 
 export default Vue.extend({
   name: "CurrentShows",
   data() {
     return {
       search: "",
-      availableShowsList: [
-        {
-          title: 'Swan Lake', 
-          date: 'Tuesday 15th - Wednesday 31st December 2019', 
-          location: 'Auditorium',
-          img: require("../../assets/swanlake.jpg")
-
-        },
-        {
-          title: "Drummers", 
-          date: 'Friday 15th - Wednesday 20th November 2019', 
-          location: 'Room II',
-          img: require("../../assets/dance.jpg")
-
-        },
-        {
-          title: "Symphony II", 
-          date: 'Tuesday 5th - Wednesday 10th December 2019', 
-          location: 'Auditorium',
-          img: require("../../assets/band.jpg")
-
-        },
-        {
-          title: "Behind Closed Curtains", 
-          date: 'Tuesday 1 - Wednesday 10 January 2020', 
-          location: 'Room I',
-          img: require("../../assets/acting.jpg")
-
-        }
-      ]
-    }
-  },
-  computed: {
-    filteredList() {
-      return this.availableShowsList.filter(show => {
-        return show.title.toLowerCase().includes(this.search.toLowerCase())
-      })
+      allAvailableShowsData: allShowsDataJSON.availableShowsList
     }
   },
   methods: {
     selectedShow(show, index) {
       this.$router.push({ 
         name: "SelectedShow", 
-        params: { title: show.title, date: show.date, location: show.location, img: show.img, index: index}
+        params: { title: show.title, index: index}
       });
     }
   }
