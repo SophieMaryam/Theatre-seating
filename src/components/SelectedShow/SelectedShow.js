47f70ced071f9ab8cee0fe6a63569008ae5805e5
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-16 15:26:04 +0100

    Refactor: Retrieve show data from json

--- src/components/SelectedShow/SelectedShow.js
+++ src/components/SelectedShow/SelectedShow.js
@@ -1,51 +1,27 @@
 import Vue from "vue";
 import firebase from "firebase";
+import allShowsDataJSON from "../../common/showList.json";
 
 export default Vue.extend({
   name: "SelectedShow",
   data() {
     return {
       showName: this.$route.params.title,
-      showImg: this.$route.params.img,
       showIndex: this.$route.params.index,
-      showDate: this.$route.params.date,
+      allAvailableShows: allShowsDataJSON.availableShowsList,
       readMore: false,
       allShows: [ 
         {
           name: "Swan Lake",
-          info: [ 
-            { id: 1, date: "Wed 21 oct", time: "20:00", cost: "from EU 25,00" },
-            { id: 2, date: "Thurs 22 oct", time: "20:00", cost: "from EU 25,00" },
-            { id: 3, date: "Fri 23 oct", time: "20:00", cost: "from EU 35,00"},
-            { id: 4, date: "Sat 24 oct", time: "20:00", cost: "from EU 45,00" }
-          ]
         },
         {
           name: "Drummers",
-          info: [ 
-            { id: 1, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00" },
-            { id: 2, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00" },
-            { id: 3, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00"},
-            { id: 4, date: "wed 22 oct", time: "20:00", cost: "from EU 26,00" }
-          ]
         },
         {
           name: "Symphony II",
-          info: [ 
-            { id: 1, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00" },
-            { id: 2, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00" },
-            { id: 3, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00"},
-            { id: 4, date: "wed 23 oct", time: "20:00", cost: "from EU 26,00" }
-          ]
         },
         {
           name: "Behind Closed Curtains",
-          info: [ 
-            { id: 1, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00" },
-            { id: 2, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00" },
-            { id: 3, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00"},
-            { id: 4, date: "wed 22 oct", time: "20:00", cost: "from EU 20,00" }
-          ]
         }
       ]
     }
--- src/components/SelectedShow/SelectedShow.vue
+++ src/components/SelectedShow/SelectedShow.vue
@@ -1,45 +1,49 @@
 <template>
   <div>
-    <div class="image1" :style="{ backgroundImage: `url(${showImg})` }"></div>
-    <div class="container bg-white tha ">
-      <div class="pl-5 pr-5 ">
-      <h1 class="bg-white pt-4 pb-3">{{ showName }}</h1>
-      <h2> {{ showDate }}</h2>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libe
-          <span
-            v-if="readMore"
-          ></span>
-          <span v-else>...</span>
-          <span v-show="readMore">sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span>
-        </p>
-      </div>
-      <div class="text-center">
-        <button class="btn" @click="readMore =! readMore">
-          <span v-if="readMore">Read Less</span>
-          <span v-else>Read More</span>
-        </button>
-      </div>
-      </div>
-      <div class="mt-5 vertical-align">
-        <table
-          class="table table-striped text-center col-7"
-          v-for="(show, indx) in allShows"
-          :key="indx"
-          id="firstTable"
-        >
-          <tbody v-if="indx == showIndex">
-            <tr class v-for="details in show.info" :key="details.id">
-              <td>{{details.date}}</td>
-              <td>{{details.time}}</td>
-              <td>{{details.cost}}</td>
-              <td>
-                <button @click="onLinkClicked(details.id, showName)">Buy Ticket</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+    <div v-for="show in allAvailableShows" :key="show.id">
+      <div v-if="showName == show.title">
+        <div
+          class="image1"
+          :style="{ backgroundImage: 'url('+require('../../assets/'+show.img)+')'}"
+        ></div>
+        <div class="container bg-white tha">
+          <div class="pl-5 pr-5">
+            <h1 class="bg-white pt-4 pb-3">{{ show.title }}</h1>
+            <h2>{{ show.date }}</h2>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libe
+                <span
+                  v-if="readMore"
+                ></span>
+                <span v-else>...</span>
+                <span
+                  v-show="readMore"
+                >sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span>
+              </p>
+            </div>
+            <div class="text-center">
+              <button class="btn" @click="readMore =! readMore">
+                <span v-if="readMore">Read Less</span>
+                <span v-else>Read More</span>
+              </button>
+            </div>
+          </div>
+          <div class="mt-5 vertical-align">
+            <table class="table table-striped text-center col-7" id="firstTable">
+              <tbody>
+                <tr class v-for="details in show.info" :key="details.id">
+                  <td>{{details.date}}</td>
+                  <td>{{details.time}}</td>
+                  <td>{{details.cost}}</td>
+                  <td>
+                    <button @click="onLinkClicked(details.id, showName)">Buy Ticket</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
