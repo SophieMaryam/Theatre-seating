4eb993fe208df2f1195bf657227a1328aa537884
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-19 10:05:56 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,3 +7,25 @@ import { config } from "./common/config.js";
 firebase.initializeApp(config);
 
 export const db = firebase.firestore();
+
+
+// export async function getUserName() {
+//     const user = firebase.auth().currentUser;
+//     return db.collection("profiles").doc(user.uid).get()
+//         .then(() => {
+//             return { success: true }
+//         })
+//         .catch((error) => {
+//             throw new Error(error)
+//         });
+// }
+
+// export async function deleteUserAccount() {
+//     const user = firebase.auth().currentUser;
+//     await db.collection("profiles").doc(user).delete()
+//     .then(() => {
+//         console.log("Document successfully deleted!");
+//     }).catch(error => {
+//         console.error("Error removing document: ", error);
+//     });
+// }
