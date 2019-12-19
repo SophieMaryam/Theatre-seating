d3cc713d54343bf9b6eb6c4a0b108dd2348ab44f
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-19 09:53:20 +0100

    Delete old code, includes Newsletter components, and update method name

--- src/components/SelectedShow/SelectedShow.js
+++ src/components/SelectedShow/SelectedShow.js
@@ -1,33 +1,22 @@
 import Vue from "vue";
 import firebase from "firebase";
 import allShowsDataJSON from "../../common/showList.json";
+import Newsletter from "../Newsletter/Newsletter.vue";
 
 export default Vue.extend({
   name: "SelectedShow",
+  components: {
+    Newsletter
+  },
   data() {
     return {
       showName: this.$route.params.title,
-      showIndex: this.$route.params.index,
       allAvailableShows: allShowsDataJSON.availableShowsList,
-      readMore: false,
-      allShows: [ 
-        {
-          name: "Swan Lake",
-        },
-        {
-          name: "Drummers",
-        },
-        {
-          name: "Symphony II",
-        },
-        {
-          name: "Behind Closed Curtains",
-        }
-      ]
+      readMore: false
     }
   },
   methods: {
-    onLinkClicked(indx, show) {
+    buyTicketForSelectedShow(indx, show) {
       const currentUser = firebase.auth().currentUser;
       if(!currentUser) {
         // Incomplete: This stills need to query theatre/indx & show name
