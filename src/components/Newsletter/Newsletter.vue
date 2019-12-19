e22504cdfd6c24b7e51635214992fdb5b4fb9e0c
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-19 09:52:48 +0100

    Create Newsletter folder and files

---
+++ src/components/Newsletter/Newsletter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-5 mb-5 container">
+    <div class="card-body background mb-2 h-100">
+      <div class="d-flex justify-content-between pl-5 pr-5">
+        <div>
+          <h3>Orius Newsletter</h3>
+          <p>Keep up to date with the latest shows.</p>
+        </div>
+        <div class="col-4">
+          <label for="email" class="text-size text-uppercase">Email Address</label>
+          <div class="d-flex">
+            <b-form-input id="input-small" size="md" class="newsletter-input" placeholder="Enter your email"></b-form-input>
+            <b-button variant="outline-secondary" class="btn btn-square ml-1 text-white">Subscribe</b-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = require("./Newsletter.js");
+</script>
+
+<style scoped src="./Newsletter.css">
+</style>
