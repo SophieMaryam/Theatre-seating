f9b9a8cb1fdcc002ba63a5c86b8732bf035abf36
SophieMaryam <sophievanwersch@hotmail.com>
2019-12-20 17:11:01 +0100

    Retrieve product info from json

--- src/components/ProductList/ProductList.vue
+++ src/components/ProductList/ProductList.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        
+  <div class="container mb-5">
+    <div class="text-center mt-5 mb-5">
+      <h1>Orius Shop</h1>
+      <h5>Buy one of our pieces!</h5>
     </div>
+    <div class="row">
+      <div class="col-xs-4 col-sm-4 col-md-4" v-for="(product, index) in productData" :key="index">
+        <div @click="goToProductPage(product)">
+          <img
+          :src="require(`../../assets/products/${product.img}`)"
+          :alt="product.alt"
+          style="width:100%; height:100%;"
+          class="img-responsive"
+        />
+        </div>
+        <div>
+          <p class="text-uppercase">{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+=
+
+<style scoped src="./ProductList.css">
+</style>
+
+<script>
+module.exports = require("./ProductList.js");
+</script>
