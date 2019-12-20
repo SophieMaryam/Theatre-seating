d3ca8d2cb493126437776ed7826c2e61c5bf5084
SophieMaryam <sophievanwersch@hotmail.com>
2019-12-20 14:04:49 +0100

    Add Get, Delete and Update User Profiles

--- src/firebase.js
+++ src/firebase.js
@@ -3,34 +3,52 @@ import "firebase/firestore";
 import "firebase/firebase-auth";
 import "firebase/storage";
 import { config } from "./common/config.js";
+import { router } from "./router/routes";
 
 firebase.initializeApp(config);
 
 export const db = firebase.firestore();
 const user = firebase.auth().currentUser;
 
-export async function getProfile() {
+export async function getUserProfileData() {
     const user = firebase.auth().currentUser;
     const querySnapshot = await db.collection("profiles").doc(user.uid).get()
     const userFullName = querySnapshot.data().name;
     return userFullName;
 }
 
+export async function getAllUserProfileData() {
+  const user = firebase.auth().currentUser;
+  const querySnapshot = await db.collection("profiles").doc(user.uid).get()
+  const userProfile = querySnapshot.data();
+  return userProfile;
+}
+
 export async function deleteUserAccount() {
-    await db.collection("profiles").doc(user).delete()
+  const user = firebase.auth().currentUser;
+    await db.collection("profiles").doc(user.uid).delete()
         .then(() => {
             console.log("Document successfully deleted!");
+            firebase.auth().signOut()
+            .then(() => {
+              router.push({name: "Home"})
+            })
+            .catch(err => console.log(err));
         }).catch(error => {
             console.error("Error removing document: ", error);
         });
 }
 
-export async function updateUserPersonalData(updatedUserData) {
+export async function updateUserPersonalData(updatedUserData) { 
     const user = firebase.auth().currentUser;
     const profileRef = await db.collection("profiles").doc(user.uid);
     return profileRef.update({
-      group: updatedUserData.name
+      name: updatedUserData.name,
+      address: updatedUserData.address,
+      postalCode: updatedUserData.postalCode,
+      phoneNumber: updatedUserData.phoneNumber
     }).then(() => {
+      console.log("woo updated")
       return {success: true}
     }).catch((error) => {
       return new Error('Error updating document: ', error)
