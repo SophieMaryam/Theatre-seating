c9e73007e8030b39314a9447e76de3854210b760
SophieMaryam <sophievanwersch@hotmail.com>
2019-12-20 17:12:06 +0100

    Retrieve data from json file

---
+++ src/components/SelectedProduct/SelectedProduct.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import allProductDataJSon from "../../common/productData.json";
+import TheatreNewsletter from "../TheatreNewsletter/TheatreNewsletter.vue";
+
+export default Vue.extend({
+    name: "SelectedProduct",
+    components: {
+        TheatreNewsletter
+    },
+    data() {
+        return {
+            productName: this.$route.params.name,
+            allProductData: allProductDataJSon.allProductData
+        }
+    }
+});
