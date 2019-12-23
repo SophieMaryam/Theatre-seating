d1a5b27dc6f071539a13b53e0f7a92b7dce9a47b
SophieMaryam <sophievanwersch@hotmail.com>
2019-12-23 11:35:27 +0100

    Add Continue Shopping Modal (not functioning)

---
+++ src/components/ContinueShoppingModal/ContinueShoppingModal.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "ContinueShoppingModal",
+    data() {
+        return {
+            data: {
+                showModal: false
+            }
+        }
+    }
+});
