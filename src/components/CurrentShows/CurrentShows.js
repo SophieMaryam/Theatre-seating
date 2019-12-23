9d046c799956f5f5c5f14ca3387bf9f25d8584ae
SophieMaryam <sophievanwersch@hotmail.com>
2019-12-23 11:36:04 +0100

    Add VueAgile slideshow component

--- src/components/CurrentShows/CurrentShows.js
+++ src/components/CurrentShows/CurrentShows.js
@@ -1,11 +1,50 @@
 import Vue from "vue";
 import allShowsDataJSON from "../../common/currentShowData.json";
+import { VueAgile } from "vue-agile";
 
 export default Vue.extend({
   name: "CurrentShows",
+  components: {
+    agile: VueAgile
+  },
   data() {
     return {
-      allAvailableShowsData: allShowsDataJSON.availableShowsList
-    }
-  }
+      allAvailableShowsData: allShowsDataJSON.availableShowsList,
+			asNavFor1: [],
+			asNavFor2: [],
+			options1: {
+				dots: false,
+				fade: true,
+				navButtons: false
+			},
+			
+			options2: {
+				autoplay: true,
+				centerMode: true,
+				dots: false,
+				navButtons: false,
+				slidesToShow: 3,
+				responsive: [
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 5
+                    }
+                },
+                
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        navButtons: true
+                    }
+                }
+            ]
+				
+			}
+		}
+	},
+  mounted () {
+		this.asNavFor1.push(this.$refs.thumbnails)
+		this.asNavFor2.push(this.$refs.main)
+	}
 });
--- src/components/CurrentShows/CurrentShows.vue
+++ src/components/CurrentShows/CurrentShows.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>
-    <div class="text-center pb-4">
-      <div class="container">
-        <h2 class="mb-3 font-weight-bold text-white pt-4">Shows</h2>
-        <div class="row">
-          <div class="col-lg-3 col-md-12 mb-4"  v-for="show in allAvailableShowsData" :key="show.id">
-            <div>
-              <img :src="require(`../../assets/${show.img}`)" class="img-fluid" :alt="show.alt">
-            </div>
-            <button class="check-btn text-white mt-3">Find Tickets</button>
+  <div class="text-center mt-5 pb-4">
+    <div class="container">
+      <h2 class="mb-3 font-weight-bold text-black">Current Shows</h2>
+      <p>Shows are currently on display at the Orius Theatre until end of this year. Tickets are available through the website.</p>
+      <div id="slideshow">
+        <agile class="main" ref="main" :options="options1" :as-nav-for="asNavFor1">
+          <div
+            class="slide"
+            v-for="(show, index) in allAvailableShowsData"
+            :key="index"
+            :class="`slide--${index}`"
+          >
+            <img :src="require(`../../assets/${show.img}`)" />
           </div>
-        </div>
+        </agile>
+        <agile class="thumbnails" ref="thumbnails" :options="options2" :as-nav-for="asNavFor2">
+          <div
+            class="slide slide--thumbniail"
+            v-for="(show, index) in allAvailableShowsData"
+            :key="index"
+            :class="`slide--${index}`"
+            @click="$refs.thumbnails.goTo(index)"
+          >
+            <img :src="require(`../../assets/${show.img}`)" />
+          </div>
+          <template slot="prevButton">
+            <i class="fas fa-chevron-left"></i>
+          </template>
+          <template slot="nextButton">
+            <i class="fas fa-chevron-right"></i>
+          </template>
+        </agile>
       </div>
     </div>
   </div>
@@ -20,7 +40,7 @@
 </style>
 
 <script>
-  module.exports = require("./CurrentShows.js");
+module.exports = require("./CurrentShows.js");
 </script>
 
 
