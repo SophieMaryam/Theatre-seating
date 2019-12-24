da3f7345b3669ceed4d710dfe1ad7c92ba069b39
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-24 12:52:47 +0100

    Get and set tabIndex in vuex store

--- src/store/store.js
+++ src/store/store.js
@@ -4,4 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    state: {
+        tabIndex: 0
+    },
+    mutations: {
+        selectedTab(state, updatedTab) {
+            state.tabIndex = updatedTab;
+        }
+    },
+    getters: {
+        tabIndex: state => state.tabIndex
+    }
 })
--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -3,26 +3,37 @@ import firebase from "firebase";
 import UserProfileDetails from "../../components/UserProfileDetails/UserProfileDetails.vue";
 import UserSettings from "../../components/UserSettings/UserSettings.vue";
 import { deleteUserAccount } from "../../firebase.js"
+
 export default Vue.extend({
   name: "ProfilePage",
   components: {
     UserProfileDetails,
     UserSettings
   },
+  computed: {
+    tabIndex: {
+      get() {
+        return this.$store.state.tabIndex;
+      },
+      set(updatedTab) {
+        this.$store.commit("selectedTab", updatedTab);
+      },
+    }
+  },
   methods: {
-    logout() {
+    logoutUser() {
       firebase.auth().signOut()
-      .then(() => {
-        this.$router.replace("login")
-      })
-      .catch(err => console.log(err));
+        .then(() => {
+          this.$router.replace("login")
+        })
+        .catch(err => console.log(err));
     },
     async deleteAccount() {
-      if(confirm("Are you sure?")) {
+      if (confirm("Are you sure?")) {
         await deleteUserAccount();
       } else {
-
+        console.error()
       }
-    },
+    }
   }
 });
