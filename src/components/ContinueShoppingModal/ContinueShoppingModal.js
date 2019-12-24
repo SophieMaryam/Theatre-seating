91341c434bfcd36f8f83e45e38fa914681a03ad1
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-24 12:58:35 +0100

    Create goToBasket and buyProduct methods

--- src/components/ContinueShoppingModal/ContinueShoppingModal.js
+++ src/components/ContinueShoppingModal/ContinueShoppingModal.js
@@ -1,12 +1,29 @@
 import Vue from "vue";
+import firebase from "firebase";
 
 export default Vue.extend({
     name: "ContinueShoppingModal",
+    props: ["productName", "productIndex", "productPrice"],
     data() {
         return {
-            data: {
-                showModal: false
+            modalOpen: false,
+            productName: "",
+            productIndex: "",
+            productPrice: ""
+        }
+    },
+    methods: {
+        goToBasket() {
+            const currentUser = firebase.auth().currentUser;
+            if (!currentUser) {
+                // Incomplete: This stills need to query theatre/indx & show name
+                this.$router.replace({ path: "/login", query: { redirect: '/theatre' } });
+            } else {
+                this.$router.replace({ path: "/profile", query: { productIndex, productName } });
             }
         }
+    },
+    buyProduct() {
+        console.log("purchase page")
     }
 });
