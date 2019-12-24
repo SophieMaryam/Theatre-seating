95ee5838c79089b5b4b29252b75a36964b2ebc02
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-24 12:59:13 +0100

    Add ContinueShoppingModal components

--- src/components/SelectedProduct/SelectedProduct.vue
+++ src/components/SelectedProduct/SelectedProduct.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <div v-for="product in allProductData" :key="product.id">
-      <div v-if="productName == product.name">
+  <div id="modalcomp">
+    <div class="d-flex justify-content-center" v-for="(product, index) in allProductData" :key="index">
+      <div v-if="productName == product.name" class="row display">
         <div
-          class="selected-product-img"
+          class="selected-product-img "
           :style="{ backgroundImage: 'url('+require('../../assets/products/'+product.img)+')'}"
         ></div>
-        <div class="container bg-white">
-          <div>
-            <h1 class="bg-white pt-4 pb-3 padding text-uppercase">{{ product.name }}</h1>
-            <h3 class="padding text-muted">{{ product.price }}</h3>
-          </div>
+        <div class="container bg-white col-5">
+          <h1 class="bg-white pt-4 pb-3 padding text-uppercase">{{ product.name }}</h1>
+          <h3 class="padding text-muted">{{ product.price }}</h3>
+          <ContinueShoppingModal :productName="product.name" :productPrice="product.price" :productIndex="index" />
         </div>
-        <b-button @click="openContinueShoppingModal(product.id, product.name)">Put in Basket</b-button>
       </div>
     </div>
     <TheatreNewsletter />
