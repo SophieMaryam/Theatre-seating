089798e5bd6fcf75e1b466e58a7ad9caad67a46d
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-24 12:59:57 +0100

    Delete unused method + add ContinueShoppingModal component

--- src/components/SelectedProduct/SelectedProduct.js
+++ src/components/SelectedProduct/SelectedProduct.js
@@ -1,21 +1,20 @@
 import Vue from "vue";
 import allProductDataJSon from "../../common/productData.json";
 import TheatreNewsletter from "../TheatreNewsletter/TheatreNewsletter.vue";
+import ContinueShoppingModal from "../ContinueShoppingModal/ContinueShoppingModal.vue";
+import { EventBus } from "../../common/EventBus.js";
 
 export default Vue.extend({
     name: "SelectedProduct",
     components: {
-        TheatreNewsletter
+        TheatreNewsletter,
+        ContinueShoppingModal
+
     },
     data() {
         return {
             productName: this.$route.params.name,
-            allProductData: allProductDataJSon.allProductData
-        }
-    },
-    methods: {
-        openContinueShoppingModal() {
-            
+            allProductData: allProductDataJSon.allProductData,
         }
     }
 });
