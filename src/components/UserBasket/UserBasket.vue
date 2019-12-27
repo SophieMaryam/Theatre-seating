b2192566f0e021f2e39504aef80537b70552323c
Sophie van Wersch <sophievanwersch@hotmail.com>
2019-12-27 10:45:24 +0100

    Display items in basket or if empty

---
+++ src/components/UserBasket/UserBasket.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-if="basketProducts == ''">
+      <h1>Your Basket is empty</h1>
+    </div>
+    <div v-else v-for="product in basketProducts" :key="product.id">
+      <h1>{{ product }}</h1>
+          <button @click="deleteProduct(product)">X</button>
+    </div>
+  </div>
+</template>
+
+<style scoped src="./UserBasket.css">
+</style> 
+
+<script>
+module.exports = require("./UserBasket.js");
+</script>
