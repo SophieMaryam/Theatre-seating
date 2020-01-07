b3248678d467b6a61899b41a3c6021f7278c3673
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-01-07 16:27:40 +0100

    Update prop name + remove index

--- src/components/SelectedProduct/SelectedProduct.vue
+++ src/components/SelectedProduct/SelectedProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="modalcomp">
-    <div class="d-flex justify-content-center" v-for="(product, index) in allProductData" :key="index">
+    <div class="d-flex justify-content-center" v-for="product in allProductData" :key="product.id">
       <div v-if="productName == product.name" class="row display">
         <div
           class="selected-product-img "
@@ -9,7 +9,7 @@
         <div class="container bg-white col-5">
           <h1 class="bg-white pt-4 pb-3 padding text-uppercase">{{ product.name }}</h1>
           <h3 class="padding text-muted">{{ product.price }}</h3>
-          <ContinueShoppingModal :productName="product.name" :singleProduct="product" />
+          <ContinueShoppingModal :selectedProduct="product" />
         </div>
       </div>
     </div>
