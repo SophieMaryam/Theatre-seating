0abdbd3d01ed161a21c997229881858aca58430f
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-01-07 16:31:14 +0100

    Temporarily place firebase snapshot in UserProfileDetails.js

--- src/components/UserProfileDetails/UserProfileDetails.js
+++ src/components/UserProfileDetails/UserProfileDetails.js
@@ -1,19 +1,26 @@
 import Vue from "vue";
-import { getAllUserProfileData } from "../../firebase.js";
+import { getAllUserProfileData, db } from "../../firebase.js";
+import firebase from 'firebase'
 
 export default Vue.extend({
   name: "UserProfileDetails",
   data() {
     return {
-      allData: {}
+      allData: {},
     }
   },
   mounted() {
     this.setProfileInfo();
   },
   methods: {
-    async setProfileInfo() {
-      this.allData = await getAllUserProfileData();
+    setProfileInfo() {
+      const user = firebase.auth().currentUser;
+      db.collection("profiles").doc(user.uid)
+        .onSnapshot((querySnapshot) => {
+          const userProfile = querySnapshot.data();
+          this.allData = userProfile
+        }
+        )
     }
   }
 });
