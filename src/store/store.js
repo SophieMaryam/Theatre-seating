ea2dce88bd3ceae5b3c8d30049f780c8ecbc027b
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-01-20 14:34:20 +0100

    Add (temp) userUserAuthenticated in store

--- src/store/store.js
+++ src/store/store.js
@@ -5,14 +5,19 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        tabIndex: 0
+        tabIndex: 0,
+        isUserAuthenticated: null
     },
     mutations: {
         selectedTab(state, updatedTab) {
             state.tabIndex = updatedTab;
+        },
+        userAuthenticationStatus(state, updatedAuthentication) {
+            state.isUserAuthenticated = updatedAuthentication;
         }
     },
     getters: {
-        tabIndex: state => state.tabIndex
+        tabIndex: state => state.tabIndex,
+        isUserAuthenticated: state => state.isUserAuthenticated
     }
 })
