9ff9e226e601322f1b1bdb94e725b76a17e9c0c0
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-02-04 14:47:14 +0100

    Temporarily disable code

--- src/components/UserBasket/UserBasket.js
+++ src/components/UserBasket/UserBasket.js
@@ -11,15 +11,31 @@ export default Vue.extend({
     mounted() {
         this.displayBasketProducts();
     },
+        // this.storageProducts = JSON.parse(localStorage.getItem("products"));
+      // let products = this.storageProducts.find(
+      //   product => product.name !== productName
+      // );
+      // if (products.quantity < 1) {
+      //   Object.assign(products, {
+      //     name: products.name,
+      //     quantity: Number(products.quantity) - 1,
+      //     price: Number(products.price) * Number(products.quantity)
+      //   });
+      //   localStorage.setItem("products", JSON.stringify(products));
+      // } else {
+      //   this.deleteProduct(product)
+      // }
+      // this.displayBasketProducts();
     methods: {
         displayBasketProducts() {
             this.basketProducts = JSON.parse(localStorage.getItem('products'));
+            console.log(this.basketProducts)
         },
         deleteProduct(productName) {
-            let storageProducts = JSON.parse(localStorage.getItem('products'));
-            let products = storageProducts.filter(product => product.name !== productName);
-            localStorage.setItem('products', JSON.stringify(products));
-            this.displayBasketProducts();
+            // let storageProducts = JSON.parse(localStorage.getItem('products'));
+            // let products = storageProducts.find(product => product.name !== productName);
+            // localStorage.setItem('products', JSON.stringify(products));
+            // this.displayBasketProducts();
         },
         goToCheckOut(productName, index) {
             this.$router.push({
