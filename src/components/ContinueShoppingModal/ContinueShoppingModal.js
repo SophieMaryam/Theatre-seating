98e2cfad7b8bb0ca696dc7ca5d1d6ed976947269
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-02-04 14:08:26 +0100

    Prevent duplicates and update quantitiy with addToBasket

--- src/components/ContinueShoppingModal/ContinueShoppingModal.js
+++ src/components/ContinueShoppingModal/ContinueShoppingModal.js
@@ -3,45 +3,48 @@ import firebase from "firebase";
 import allProductDataJSon from "../../common/productData.json";
 
 export default Vue.extend({
-    name: "ContinueShoppingModal",
-    props: ["selectedProduct"],
-    data() {
-        return {
-            modalOpen: false,
-            allProductData: allProductDataJSon.allProductData,
-        }
+  name: "ContinueShoppingModal",
+  props: ["selectedProduct"],
+  data() {
+    return {
+      modalOpen: false,
+      allProductData: allProductDataJSon.allProductData
+    };
+  },
+  methods: {
+    addToBasket(product) {
+      let products = JSON.parse(localStorage.getItem("products"));
+      let filteredProduct = products.find(p => p.name == product.name);
+      if (filteredProduct) {
+        Object.assign(filteredProduct, {
+          name: product.name,
+          quantity: Number(filteredProduct.quantity) + 1,
+          price: Number(product.price) * (Number(filteredProduct.quantity) + 1),
+        });
+      } else {
+        products.push({
+          name: product.name,
+          price: product.price,
+          quantity: 1
+        });
+      }
+      return localStorage.setItem("products", JSON.stringify(products));
     },
-    methods: {
-        addToBasket(product) {
-            let products = [];
-            if (localStorage.getItem('products')) {
-                products = JSON.parse(localStorage.getItem('products'));
-            }
-            // products.push({ name: product.name, image: product.img, price: product.price });
-            // localStorage.setItem('products', JSON.stringify(products));
-            let hello = products.map(name => name.name)
-            const values = hello.forEach( (item) => {
-                if(item == product.name) {
-                    console.log("hi")
-                } else {
-                    products.push({ name: product.name, image: product.img, price: product.price });
-                    localStorage.setItem('products', JSON.stringify(products));
-                }
-              });
-
-        },
-        goToBasket(productName) {
-            const currentUser = firebase.auth().currentUser;
-            if (!currentUser) {
-                // Incomplete: This stills need to query theatre/indx & show name
-                this.$router.replace({ path: "/login", query: { redirect: '/checkout' } });
-            } else {
-                this.$store.commit("selectedTab", 3);
-                this.$router.replace({ path: "/profile", query: { productName } });
-            }
-        },
-        goToCheckOut(productName) {
-            this.$router.replace({ path: "/checkout", query: { productName } });
-        }
+    goToBasket(productName) {
+      const currentUser = firebase.auth().currentUser;
+      if (!currentUser) {
+        // Incomplete: This stills need to query theatre/indx & show name
+        this.$router.replace({
+          path: "/login",
+          query: { redirect: "/checkout" }
+        });
+      } else {
+        this.$store.commit("selectedTab", 3);
+        this.$router.replace({ path: "/profile", query: { productName } });
+      }
+    },
+    goToCheckOut(productName) {
+      this.$router.replace({ path: "/checkout", query: { productName } });
     }
+  }
 });
