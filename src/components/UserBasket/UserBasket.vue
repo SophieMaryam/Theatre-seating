230fae90dfeae39003c8996290e9896d69579cf7
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-02-05 11:25:28 +0100

    Add product image

--- src/components/UserBasket/UserBasket.vue
+++ src/components/UserBasket/UserBasket.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
     <div>
-    <div v-if="basketProducts.length === 0">
-      <h1>Your Basket is empty</h1>
-    </div>
-    <div v-for="(product, index) in basketProducts" :key="index" v-else>
-      <h1>{{ product.name }} {{product.price }}</h1>
-      <b-button @click="deleteProduct(product.name)">X</b-button>
-      <b-button @click="reduceProductAmount(product)">Decrease</b-button>
-      <b-button @click="goToCheckOut(product.name, index)">Buy Product</b-button>
-    </div>
+      <div v-if="basketProducts.length === 0">
+        <h1>Your Basket is empty</h1>
+      </div>
+      <div v-for="(product, index) in basketProducts" :key="index" v-else>
+        <div
+          class="selected-product-img "
+          :style="{
+            backgroundImage:
+              'url(' + require('../../assets/products/' + product.img) + ')'
+          }"
+        ></div>
+        <h1>{{ product.name }} {{ product.price }}</h1>
+        <b-button @click="deleteProduct(product.name)">X</b-button>
+        <b-button @click="reduceProductAmount(product)">Decrease</b-button>
+        <b-button @click="goToCheckOut(product.name, index)"
+          >Buy Product</b-button
+        >
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped src="./UserBasket.css">
-</style>
+<style scoped src="./UserBasket.css"></style>
 
 <script>
 module.exports = require("./UserBasket.js");
