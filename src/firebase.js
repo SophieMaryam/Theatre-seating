60bc74bba78bc5ede5bb7cc4712ce51f18b402c8
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-02-26 11:05:24 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -20,54 +20,103 @@ export const db = firebase.firestore();
 
 export async function getAllUserProfileData() {
   const user = firebase.auth().currentUser;
-  const querySnapshot = await db.collection("profiles").doc(user.uid).get()
+  const querySnapshot = await db
+    .collection("profiles")
+    .doc(user.uid)
+    .get();
   const userProfile = querySnapshot.data();
   return userProfile;
 }
 
 export async function deleteUserAccount() {
   const user = firebase.auth().currentUser;
-  await db.collection("profiles").doc(user.uid).delete()
+  await db
+    .collection("profiles")
+    .doc(user.uid)
+    .delete()
     .then(() => {
-      firebase.auth().signOut()
+      firebase
+        .auth()
+        .signOut()
         .then(() => {
-          router.push({ name: "HomePage" })
-        })
-    }).catch(error => {
+          router.push({ name: "HomePage" });
+        });
+    })
+    .catch(error => {
       console.error("Error removing document: ", error);
     });
 }
 
 export async function updateUserPersonalData(updatedUserData) {
   const user = firebase.auth().currentUser;
-  await db.collection("profiles").doc(user.uid).update({
-    name: updatedUserData.name,
-    address: updatedUserData.address,
-    postalCode: updatedUserData.postalCode,
-    phoneNumber: updatedUserData.phoneNumber
-  })
+  await db
+    .collection("profiles")
+    .doc(user.uid)
+    .update({
+      name: updatedUserData.name,
+      address: updatedUserData.address,
+      postalCode: updatedUserData.postalCode,
+      phoneNumber: updatedUserData.phoneNumber
+    });
 }
 
-export async function userLogin(email, password, value) {
-  firebase.auth().signInWithEmailAndPassword(email, password)
+export async function userLogin(email, password, queryRedirect) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
     .then(() => {
-      router.push(value || '/profile')
+      router.push(queryRedirect || "/profile");
+    })
+    .catch(err => console.log(err));
+}
+
+export async function userGoogleLogin(queryRedirect) {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(() => {
+      this.$router.push(queryRedirect || "/profile");
     })
     .catch(err => console.log(err));
 }
 
 export async function userRegistration(email, password, fullName) {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
     .then(user => {
-      db.collection("profiles").doc(user.uid).set({
-        email: email,
-        name: fullName,
-        address: "",
-        postalCode: "",
-        household: "",
-        phoneNumber: "",
-        purchasedProducts: []
-      })
-      router.replace("profile")
-    })
+      db.collection("profiles")
+        .doc(user.user.uid)
+        .set({
+          email: email,
+          name: fullName,
+          password: password,
+          address: "",
+          postalCode: "",
+          household: "",
+          phoneNumber: "",
+          tickets: [
+            {
+              show: "",
+              time: "",
+              price: "",
+              seat: {
+                row: "",
+                section: "",
+                seatNumber: ""
+              }
+            }
+          ]
+        });
+      router.replace("profile");
+    });
+}
+
+export async function userLogout() {
+  firebase.auth().signOut()
+  .then(() => {
+    this.$router.replace("login")
+  })
+  .catch(err => console.log(err));
 }
