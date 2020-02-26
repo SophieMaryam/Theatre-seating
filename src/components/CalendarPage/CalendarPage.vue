7c18b7ffaac03b12cb2d4ff336043de3960addd7
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-02-26 13:09:27 +0100

    Update button to b-button

--- src/components/CalendarPage/CalendarPage.vue
+++ src/components/CalendarPage/CalendarPage.vue
@@ -2,31 +2,31 @@
   <div class="pt-5 pb-5">
     <div class="container col-7">
       <div class="pad-15-hor pad-15-ver pb-2 search-parent">
-        <div class="col 6 ml-auto text-uppercase font-weight-bold text-center"> 
-          <h2> Calendar </h2> 
+        <div class="col 6 ml-auto text-uppercase font-weight-bold text-center">
+          <h2> Calendar </h2>
         </div>
         <div class="d-flex mt-5 mb-2">
           <b-form-input
             v-model="search"
             type="text"
             placeholder="Search on shows or artists"
-          ></b-form-input> 
+          ></b-form-input>
            <font-awesome-icon icon="search" class="icons ml-4 mt-2" />
         </div>
       </div>
       <div class="pb-2">
-        <div class="card-body background d-flex mb-2 h-100" v-for="(show, index) in filteredList()" :key="index">
+        <div class="card-body background d-flex mb-2 h-100" v-for="(show, index) in filteredShowList()" :key="index">
           <div class="image">
             <img class="image" :src="require(`../../assets/${show.img}`)" />
           </div>
           <div class="ml-3">
             <h2 class="title"> {{ show.title }} </h2>
-            <p class="date"> {{ show.date }}
+            <p class="date"> {{ show.dates }}
               <span> {{ show.location }} </span>
             </p>
           </div>
           <div class="text-uppercase ml-2">
-            <b-button @click="selectedShow(show, index)" class="btn text-black mt-2 text-uppercase font-weight-bold">Order Tickets </b-button> 
+            <b-button @click="selectedShow(show, index)" class="btn text-black mt-2 text-uppercase font-weight-bold">Order Tickets</b-button>
             <b-button class="btn text-black mt-2 text-uppercase font-weight-bold">More info</b-button>
           </div>
         </div>
@@ -40,4 +40,4 @@
 
 <script>
   module.exports = require("./CalendarPage.js");
-</script> 
+</script>
